d9d62e9c6c422b0ad0db8194daca17283fedea8b
Rodolfo Gonzalez <rodoart@ciencias.unam.mx>
2022-05-29 23:53:06 -0500

    More compact drive management.

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_module_name }}/utils/paths.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_module_name }}/utils/paths.py
@@ -1,5 +1,11 @@
+from ast import Str
 from pathlib import Path
-from typing import Callable
+from typing import (
+    Callable
+)
+from xmlrpc.client import Boolean
+
+from grpc import Call
 
 def make_dir_function(
     dir_name= '',
@@ -52,3 +58,102 @@ for dir_type in [
     ]:
     dir_var = '_'.join(dir_type) + "_dir"
     exec(f"{dir_var} = make_dir_function({dir_type})")
+
+from os import listdir
+from os.path import (exists, isfile)
+
+def is_valid(
+    path: Path
+) -> bool:
+    """
+    Function to Check if the path specified specified is an existent
+    non empty directory
+    """
+    if exists(path) and not isfile(path):
+
+        # Checking if the directory is empty or not
+        if  len(listdir(path))!=0:
+            return True
+        else:
+            return False
+    
+    else: 
+        return False
+
+
+
+from typing import (Callable, Sequence)
+from distutils.dir_util import copy_tree
+from os import listdir
+from shutil import copy
+
+
+def make_remote_copy_of_workspace_functions(
+    local_path = '',
+    remote_path = '', 
+    notebook_path = '',
+) -> Sequence[Callable[... , None]]:
+    '''
+    Generates functions to generate updates to files from another folder on the 
+    same machine to the folder where this notebook is located, following the 
+    project structure. 
+    A different path can be specified for the current notebook folder.
+
+    Args:
+        remote_path:
+        notebook_path:
+        
+    '''
+
+
+
+    local_dir = make_dir_function(workspace=local_path)
+    remote_dir = make_dir_function(workspace = remote_path)
+
+    def update_from_remote(): 
+        if local_dir() != remote_dir() and is_valid(remote_dir()):
+            copy_tree(str(remote_dir()), str(local_dir()))
+            print('The remote files have been copied to the local repository.')
+        
+        else:
+            print('There is no need to copy the remote files, '+
+                  'as the remote repository does not exist or is empty.')
+
+    def update_to_remote():
+        if local_dir() == remote_dir():
+            return None
+
+        exception_list = ['notebooks',  '.config', '.git', 'sample_data' ]
+       
+        local_file_names = listdir(local_dir())
+        for name in local_file_names:
+            if (local_dir(name).is_dir() and name not in exception_list):
+                copy_tree(str(local_dir(name)),
+                        str(remote_dir(name)))
+    
+
+    def update_notebook(to_remote=False):
+        if not notebook_path:
+            return None
+    
+        file_path = Path(notebook_path).resolve()
+
+        if to_remote:
+            copy(file_path, remote_dir('notebooks', file_path.name))
+        else:
+            copy(local_dir('notebooks', file_path.name), file_path)            
+        
+        print(f'The notebook {file_path.name} has been updated in the' + 
+                ' remote folder')
+
+    
+    return local_dir, update_from_remote, update_to_remote, update_notebook
+
+
+
+
+
+
+        
+
+
