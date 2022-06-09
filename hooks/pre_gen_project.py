122e93d994a273ffdecca1c9010740fdc401b569
Rodolfo Gonzalez <rodoart@ciencias.unam.mx>
2022-06-08 22:15:57 -0500

    Using - rather than _

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,6 +3,6 @@ from cookiecutter.main import cookiecutter
 from datetime import datetime
 
 cookiecutter(
-    'cookiecutter_conda_data_science',
+    'cookiecutter-conda-data-science',
     extra_context={'timestamp': datetime.utcnow().isoformat()}
 )
